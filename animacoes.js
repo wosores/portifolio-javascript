feba6a812d58e2364c4d8151aad51ba1c43e0241
William da Silva Osores <wosores@yahoo.com.br>
2021-05-16 17:35:39 -0300

    Adicionado em 052021

---
+++ animacoes.js
@@ -0,0 +1,99 @@
+function initTabNav(){
+    const tabMenu=document.querySelectorAll('[data-tab="menu"] li');
+    const tabContent = document.querySelectorAll('[data-tab="content"] section');
+
+    if(tabMenu.length && tabContent.length){
+        tabContent[0].classList.add('ativo');
+        
+        function activeTab(index){
+            tabContent.forEach((section) => {
+                section.classList.remove('ativo');
+            });
+            const direcao = tabContent[index].dataset.anime;
+            tabContent[index].classList.add('ativo',direcao);
+        }
+        tabMenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click',() =>{
+                activeTab(index);
+            });
+       });
+    }
+}
+initTabNav();
+
+function initAccordion(){
+    const accordionList=document.querySelectorAll('[data-anime="accordion"] dt');
+    if(accordionList.length){
+    accordionList[0].classList.toggle('ativo');
+    accordionList[0].nextElementSibling.classList.toggle('ativo');
+
+    function ActiveAccordion(){
+        this.classList.toggle('ativo');
+        this.nextElementSibling.classList.toggle('ativo');
+    }
+
+    accordionList.forEach((item) => {
+        item.addEventListener('click',ActiveAccordion);
+    });
+}
+};
+
+initAccordion();
+
+function initScrollSuave(){
+
+    const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]');
+
+    function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+        //FORMA ANTIGA DE FAZER SCROOLL SUAVE
+        // section.scrollIntoView({
+        //     behavior: 'smooth',
+        //     block:'start',
+        // });
+
+        //FORMA NOVA NÃO COMPATIVEL COMO TODOS NAVEGADORES
+        const topo = section.offsetTop;
+        window.scrollTo({
+            top: topo,
+            behavior: 'smooth',
+        });
+    }
+
+    linksInternos.forEach((link) => {
+        link.addEventListener('click', scrollToSection);
+    });
+}
+
+initScrollSuave();
+
+function initAnimacaoScroll(){
+
+const sections=document.querySelectorAll('[data-anime="scroll"]');
+const sections2=document.querySelectorAll('[data-anime="scroll2"]');
+const windowMetade = window.innerHeight * 0.6;
+
+
+function animaScroll(){
+    sections.forEach((section) => {
+        const sectionTop=section.getBoundingClientRect().top;
+        const isSectionVisible=(sectionTop - windowMetade) <0;
+        if(isSectionVisible){
+            section.classList.add('ativo');
+        }
+    })
+    sections2.forEach((section) => {
+        const sectionTop=section.getBoundingClientRect().top;
+        const isSectionVisible=(sectionTop - windowMetade) <0;
+        if(isSectionVisible){
+            section.classList.add('ativo');
+        }
+    })
+}
+animaScroll()
+
+window.addEventListener('scroll',animaScroll)
+}
+initAnimacaoScroll();
---
+++ script.js
@@ -0,0 +1,19 @@
+//usando dataset para selecionar div ou o atributo
+//const div =document.querySelector('div');
+
+//selecionar o atributo
+// const div =document.querySelector('[data-cor]');
+// console.log(div.dataset)
+
+//adicionando nova propriedade
+// div.dataset.widht = 100;
+// div.dataset.totalHeigth='2000'
+//delete div.dataset.width
+
+//exercicios
+//Adicionar atributo data-anime="show-down" e data-anime="show-right"  todos
+//os sections com descricao das liguagens.
+// const sections=document.querySelectorAll('section')
+// console.log(sections)
+// sections.dataset.dataAnime="show-down"
+// sections.dataset.dataAnime="show-right"
